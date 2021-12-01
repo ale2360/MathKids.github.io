9fa3e12fa20209bda7ba97cee9028a8d5e01b38a
ale2360 <acxel333@gmail.com>
2021-12-01 17:09:16 -0600

    Actualizacion1

---
+++ js/init.js
@@ -0,0 +1,18 @@
+/*  Inicializa Firebase con la
+ * configuración del proyecto.
+ * Revisa la configuración en tu
+ * servidor de Firebase y cópiala
+ * aquí sustituyendo los
+ * asteriscos. Los campos deben
+ * quedar igual que en tu
+ * servidor. */
+// @ts-ignore
+firebase.initializeApp({
+  apiKey: "AIzaSyBqjw4u9gsZfaVi7gdqiw3hCo7P4cuZGCA",
+  authDomain: "mathmx-dd8cd.firebaseapp.com",
+  projectId: "mathmx-dd8cd",
+  storageBucket: "mathmx-dd8cd.appspot.com",
+  messagingSenderId: "98772461089",
+  appId: "1:98772461089:web:e0c3f46983a55ad566e560",
+  measurementId: "G-092NT5YYGH"
+});
