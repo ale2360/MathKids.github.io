57bbb28033dc45f82503da8b8b278943e4268ad9
ale2360 <acxel333@gmail.com>
2021-12-05 00:20:42 -0600

    act23

--- js/seguridad.js
+++ js/seguridad.js
@@ -37,23 +37,24 @@ export async function
  * @param {string[]} roles
  * @returns {Promise<boolean>} */
 export async function
-  tieneRol(usuario, roles) {
-  if (usuario && usuario.email) {
-    const rolIds =
-      await cargaRoles(
-        usuario.email);
-    for (const rol of roles) {
-      if (rolIds.has(rol)) {
-        return true;
-      }
-    }
-    alert("No autorizado.");
-    location.href = "index.html";
-  } else {
-    iniciaSesión();
-  }
-  return false;
-}
+  tieneRol(usuario, roles) 
+//   {
+//   if (usuario && usuario.email) {
+//     const rolIds =
+//       await cargaRoles(
+//         usuario.email);
+//     for (const rol of roles) {
+//       if (rolIds.has(rol)) {
+//         return true;
+//       }
+//     }
+//     alert("No autorizado.");
+//     location.href = "index.html";
+//   } else {
+//     iniciaSesión();
+//   }
+//   return false;
+// }
 
 export async function
   terminaSesión() {
