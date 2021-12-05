54a5ff421dfc9fc3b03108f7b694d239abc184e3
ale2360 <acxel333@gmail.com>
2021-12-05 00:29:25 -0600

    act1

--- cmp/mi-nav.js
+++ cmp/mi-nav.js
@@ -39,7 +39,7 @@ class MiNav extends HTMLElement {
       usu */
       async cambiaUsuario(usu) {
         if (usu && usu.email) {
-          let html = "";
+          let html = "ul";
           const roles =
             await cargaRoles(
               usu.email);
