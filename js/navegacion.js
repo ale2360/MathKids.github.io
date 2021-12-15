73d91605a507e306217850aeeeb98a10d1995492
ale2360 <acxel333@gmail.com>
2021-12-14 21:27:21 -0600

    hjghj

--- js/navegacion.js
+++ js/navegacion.js
@@ -1,10 +1,17 @@
 export function
   muestraAlumnos() {
   location.href =
-    "#";
+    "juego.html";
 }
 
 export function
   muestraUsuarios() {
   location.href = "usuarios.html";
 }
+
+
+// export function
+//   muestraAlumnos() {
+//   location.href =
+//     "alumnos.html";
+// }
